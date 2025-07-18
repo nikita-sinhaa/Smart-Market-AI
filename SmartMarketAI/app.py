06f46f996189be32a13e19a70a802f8d7d84c2ab
Nikita Sinha <nikitaa.sinhaa@gmail.com>
2025-07-18 16:20:24 -0400

    Delete SmartMarketAI directory

--- SmartMarketAI/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit.web.cli as stcli
-import sys
-
-def main():
-    sys.argv = ["streamlit", "run", "frontend/dashboard.py"]
-    sys.exit(stcli.main())
-
-if __name__ == "__main__":
-    main()
